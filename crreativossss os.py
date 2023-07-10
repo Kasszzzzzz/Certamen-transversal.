0b2ee7231211b5172b47497fbdc1e580bfe81c27
Alvaro Sanchez <137190598+Kasszzzzzz@users.noreply.github.com>
2023-07-10 11:19:24 -0400

    Add files via upload

---
+++ crreativossss os.py
@@ -0,0 +1,148 @@
+import os
+os.system ("cls")
+cantPlatinum = 0
+cantGold = 0
+cantSilver = 0
+platinum = 0
+gold = 0
+Silver = 0
+opcEntrada = 0
+menu = 0
+compraEntrada = 0
+Ubicacion = 0
+Asistentes = 0
+rut = 0
+ganancias = 0
+salir = 0
+
+
+while True:
+    try:
+        opc = int(input(menu))
+        if opc == 0:
+            break
+        elif opc ==1:
+            compraEntrada()
+        elif opc ==2:
+            Ubicacion()
+        elif opc == 3:
+            Asistentes
+        elif opc ==4:
+            salir
+
+      
+      try:
+        print("MENU DE CREATIVOS.CL , HOY MICHAEL JAM")
+    print("""
+    1|Compra de entradas
+    2|Mostar ubicaciones disponibles
+    3|Ver Listado de asistentes
+    4|Mostar ganancias totales
+    5|Salir del menu""")
+
+while True:
+    print("A escogido comprar entradas, estas son las disponibles")
+    print("""
+    1|Platinum $120.000
+    2|Gold$80.000
+    3|Silver $50.000""")
+    try:
+    
+        opcEntrada = int(input("Ingrese su opcion deseada."))
+        if opcEntrada == 1:
+            print("A comprado entrada Platinum por $120.000")
+            platinum = True
+            break
+        elif opcEntrada == 2:
+            print ("A comprado la entrada Gold por $80.000")
+            gold = False
+            break
+        elif opcEntrada == 3:
+            print ("A comprado la entrada Silver por $50.000")
+            break
+        else:
+            print("Opcion invalida.")
+    except:
+        print("Excepcion de entradas")
+
+while True:
+    print("2|Mostrar ubicaciones disponibles del escenario.")
+    try:
+        else:
+    except:
+        
+        def listadoDeAsistentes():
+            print("Opcion 3")
+            print("Listado de asistentes \n")
+            print("RUT|    ")
+            print("--+-------")
+        rut = 0
+        for i in range(len(rut)):
+            if
+        else:
+        while True:
+            try:
+                cantEntradas = int(input("Cantidad de entradas: "))
+                if cantEntradas > 0 :
+                    if platinum == True:
+                        platinum  = cantPlatinum + cantEntradas
+                        platinum = cantEntradas  * 120000
+                        break
+                    elif gold == False:
+                        cantEntradas = cantGold + cantEntradas
+                        gold = cantEntradas * 80000
+                        break
+                    elif Silver == False:
+                        Silver = Silver + cantEntradas
+                        Silver = cantEntradas * 50000
+            def gananciasTotales():
+                print("Mostrar ganancias totales:  ")
+                print("Ganancias Totales\n")
+                print("TIPO ENTRADA| CANTIDAD | TOTAL")
+                print("-------+--------+-----------")
+            cantEntradas = 0
+            for i in range (len(ganancias)):
+                if ganancias[i]:
+                else:
+                print(f"{i+1} | {opcEntrada[i]:4d} | {cantEntradas[i]:20s}| {ganancias[i]:5s}")  
+                print("----------------------------------------------------------------------")  
+                print(f"Total del todo: {i+1} ")
+
+        while True:
+    try:
+        opc = int(input(menu))
+        if opc == 0:
+            break
+        elif opc == 1:
+            opcEntrada()                  
+        elif opc == 2:
+            Ubicacion()
+        elif opc == 3:
+            Asistentes()
+            print()
+        elif opc ==4:
+            ganancias()
+        elif opc == 5:
+            salir   
+
+
+
+
+
+            
+
+
+
+     
+
+      
+
+
+
+
+        
+
+    
+
+
+    
